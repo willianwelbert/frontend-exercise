511b12bd4b7aade4c2e894835a1d68cd780a6ba4
Willian Welbert <willianwelbert@yahoo.com.br>
2021-05-12 12:10:46 -0300

    :art: cleans comments and renames functions

--- src/App.js
+++ src/App.js
@@ -13,21 +13,13 @@ function App() {
     const inputSize = dividedInput.length;
     const dictionary = invertedDictionary();
 
-    //
-    //token array positions are 'million', 'thousand', 'hundred'
-    //those can help me identify output size
-    //
-
-    //from the top:
-    //zero always returns 0
-
     const oneWord = (input) => {
       return dictionary[input]
         ? dictionary[input]
         : "Sorry, we could not find that number";
     };
 
-    const twoWords = (input) => {
+    const multipleWords = (input) => {
       const reducer = (acc, current) => {
         const key = dictionary[current] || 0;
         const numericKey = Number(key);
@@ -36,45 +28,9 @@ function App() {
       return input.reduce(reducer, 0);
     };
 
-    // const threeWords = (input) => {
-    //   const numbersOnly = input.filter((entry) => entry !== "and");
-
-    //   const reducer = (acc, current) => {
-    //     const key = dictionary[current] || 0;
-    //     const numericKey = Number(key);
-    //     return key.length > 2 ? acc * numericKey : acc + numericKey;
-    //   };
-    //   return numbersOnly.reduce(reducer, 0);
-    // };
-
-    switch (inputSize) {
-      case 1:
-        return setOutput(oneWord(dividedInput));
-      case 2:
-        return setOutput(twoWords(dividedInput));
-      case 3:
-        return setOutput(twoWords(dividedInput));
-      default:
-        return setOutput(twoWords(dividedInput));
-    }
-
-    //three words is where we start to get dirty
-    // one hundred and twenty two
-    // 'one hundred' could be read as 1 input, 'and' is ignored (filter?)
-    //so grabing 'hundred' and then index -1 should give me a searchable number
-    //that number * 100
-    // 1 * 100 + 20 + 2 => 100 + 20 + 2 => 122
-
-    //from the logic above is just a matter of token vs multiplication factor
-    // [1] * 1000 = thousand
-    // [1] * 100000 = million
-    // where [1] is number before token (index -1)
-    // and token is a dictionary item
-    // 9 * thousand + 20 + 2
-    // 9 * 1000 + 20 + 2
-    // 9000 + 20 +2
-    // 9022
-    // nine thousand twenty two
+    inputSize > 1
+      ? setOutput(multipleWords(dividedInput))
+      : setOutput(oneWord(dividedInput));
   };
 
   const numberToInFull = (input) => {
